b659d9270e8a2ef107dce8fa69746b8acdcaecc4
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2021-06-27 14:11:47 +0200

    useffect app

---
+++ useeffect/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    console.log('render')
+  })
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
