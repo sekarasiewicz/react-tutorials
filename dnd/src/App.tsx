779f25eafa9268d075a8907e051c7c27fcf65119
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2022-05-08 11:31:36 +0200

    change background-color on drag

--- dnd/src/App.tsx
+++ dnd/src/App.tsx
@@ -3,6 +3,8 @@ import { Column } from "./components";
 import { initialData, InitialDataType } from "./data/initialData";
 import {
   DragDropContext,
+  DragStart,
+  DragUpdate,
   DropResult,
   ResponderProvided,
 } from "react-beautiful-dnd";
@@ -11,6 +13,8 @@ const App: React.FC = () => {
   const [state, setState] = useState<InitialDataType>(initialData);
 
   const handleDragEnd = (result: DropResult, provided: ResponderProvided) => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
     const { draggableId, source, destination } = result;
 
     if (!destination) {
@@ -38,8 +42,28 @@ const App: React.FC = () => {
     setState(newState);
   };
 
+  const handleDragStart = (start: DragStart, provided: ResponderProvided) => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2s ease";
+  };
+
+  const handleDragUpdate = (
+    update: DragUpdate,
+    provided: ResponderProvided
+  ) => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  };
+
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
+    <DragDropContext
+      onDragEnd={handleDragEnd}
+      onDragStart={handleDragStart}
+      onDragUpdate={handleDragUpdate}
+    >
       {state.columnOrder.map((columnId) => {
         const column = state.columns[columnId];
         const tasks = column.taskIds.map((column) => state.tasks[column]);
