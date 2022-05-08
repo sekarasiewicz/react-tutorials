116910982b274c5134cb951fed480dde8320aad1
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2022-05-08 10:20:56 +0200

    Remove strict mode so dnd can work with react 18

--- dnd/src/components/Task.styles.ts
+++ dnd/src/components/Task.styles.ts
@@ -5,4 +5,5 @@ export const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
+  background-color: white;
 `;
--- dnd/src/index.tsx
+++ dnd/src/index.tsx
@@ -9,10 +9,10 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
+  <>
     <Reset />
     <App />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
