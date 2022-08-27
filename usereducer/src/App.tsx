6353730f4566474d6023cfa441ea66be0025b3da
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2022-08-27 22:10:01 +0200

    simple example with payload

--- usereducer/src/App.tsx
+++ usereducer/src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useReducer } from 'react'
 import './App.css';
 
-const reducer = (state: {count: number}, action: {type: 'INCREMENT' | 'DECREMENT'}) => {
+const reducer = (state: {count: number}, action: {type: 'INCREMENT' | 'DECREMENT' | 'CUSTOM', payload?: number}) => {
   switch (action.type) {
     case 'INCREMENT':
       return {count: state.count + 1};
     case 'DECREMENT':
       return {count: state.count - 1};
+    case 'CUSTOM':
+      return {count: state.count + (action?.payload || 0)};
     default:
       throw new Error('Unexpected action');
   }
@@ -22,11 +24,17 @@ function App() {
   const decrement = () => {
     dispatch({type: 'DECREMENT'});
   }
+
+  const custom = () => {
+    dispatch({type: 'CUSTOM', payload: 5});
+  }
+
   return (
     <>
       <button onClick={decrement}>-</button>
       <span>{state.count}</span>
       <button onClick={increment}>+</button>
+      <button onClick={custom}>custom</button>
     </>
   );
 }
